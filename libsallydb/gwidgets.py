257c9479bf401c114ce33e8281d3ff93fd1530cc
dav <fds>
2014-04-12 16:24:19 -0300

    table is working

--- libsallydb/gwidgets.py
+++ libsallydb/gwidgets.py
@@ -11,121 +11,123 @@ from gi.repository import Gtk
 from gi.repository import Pango
 
 
+def parets_states(parents, states):
+    """
+
+    :param parents: list of parent names
+    :param states: disctionary with k=vertex names and v=list of states
+    """
+    parents_matrix = []
+    for par in parents:
+        p_sts = states[par]
+
+        if not parents_matrix:
+            parents_matrix = [[s] for s in p_sts]
+        else:
+            new_par_matrix = []
+            # previous parents
+            for prev_p in parents_matrix:
+                for pstate in p_sts:
+                    tmp = list(prev_p)
+                    tmp.append(pstate)
+                    new_par_matrix.append(tmp)
+
+            parents_matrix = new_par_matrix
+    return parents_matrix
 
 
+def create_treeview_for_cpt(vertices, edges, states, query_v):
+    """
+    Create a tree view for the CPT of the node query_v
+    :param vertices:
+    :param edges:
+    :param states:
+    :param query_v:
+    :return:
+    """
+    ### Detect parents
+    parents = []
+    for v1, v2 in edges:
+        if v2 == query_v:
+            parents.append(v1)
 
-class CptTable(Gtk.ApplicationWindow):
-    def __init__(self, app, vertices, edges, states, query_v):
-        Gtk.Window.__init__(self, title="CPT for " + query_v, application=app)
-        self.set_default_size(250, 100)
-        self.set_border_width(10)
+    # numeric columns
+    n_state_cols = len(states[query_v])
+    # Columns for titles
+    n_parent_cols = len(parents)
 
-        ### Detect parents
-        parents = []
-        for v1, v2 in edges:
-            if v2 == query_v:
-                parents.append(v1)
+    # TODO Validate parents not zero states
+    # number of rows
+    n_rows = 1
+    for p in parents:
+        n_rows *= len(states[p])
 
-        # numeric columns
-        n_state_cols = len(states[query_v])
-        # Columns for titles
-        n_parent_cols = len(parents)
+    # data taypes for parents
+    parent_types = [str for i in parents]
+    # the data in the model (three strings for each row, one for each column)
+    cpt_types = [str for i in states[query_v]]
+    # all parent + cpt types
+    all_types = parent_types + cpt_types
 
-        # TODO Validate parents not zero states
-        # number of rows
-        rows = 1
-        for p in parents:
-            rows *= len(states[p])
-        print parents, "-", states[query_v]
+    print all_types
 
-        # data taypes for parents
-        parent_types = [str for i in parents]
-        # the data in the model (three strings for each row, one for each column)
-        cpt_types = [str for i in states[query_v]]
-        # all parent + cpt types
-        all_types = parent_types + cpt_types
+    ### DATA MODEL
+    model = Gtk.ListStore(*all_types)
 
-        print all_types
-        # cpt_models[1].append(["x"])
+    # FILL DATA
+    parents_matrix = parets_states(parents, states)
+    state_vals = ["0.0"] * n_state_cols
 
-        model = Gtk.ListStore(*all_types)
-        model.append(["x", "y", str(1.0), str(2.0)])
+    print "result="
+    for l in parents_matrix:
+        model.append(l + state_vals)
 
+    # self.set_model = model
+    view = Gtk.TreeView(model)
 
-        view = Gtk.TreeView(model)
+    # Table titles
+    table_titles = parents + [s + "(%)" for s in states[query_v]]
 
-        # Table titles
-        table_titles = parents + [s + "(%)" for s in states[query_v]]
+    editable_cells = {}
 
-        editable_cells = {}
-        # for each column
-        for i in range(len(table_titles)):
-            # cellrenderer to render the text
-            cell = Gtk.CellRendererText()
+    # Format for each column
+    for i in range(len(table_titles)):
+        # cellrenderer to render the text
+        cell = Gtk.CellRendererText()
 
-            # Title in bold
-            if i < n_parent_cols:
-                cell.props.weight_set = True
-                cell.props.weight = Pango.Weight.BOLD
-                cell.props.background = "gray"
+        # Title in bold
+        if i < n_parent_cols:
+            cell.props.weight_set = True
+            cell.props.weight = Pango.Weight.BOLD
+            cell.props.background = "gray"
 
-            else:
-                cell.props.xalign = 1.0
-                cell.props.editable = True
+        else:
+            cell.props.xalign = 1.0
+            cell.props.editable = True
+            editable_cells[cell] = i
+            ## Event for Edited cells
+            def text_edited(widget, path, text):
+                #1 remove the % symbol and spaces
+                # text = text.replace("%","")
+                # text = text.replace(" ","")
+                print text
+                #2 validate the number between 0 and 100
+                val = float(text)
+                if val < 0 or val > 100:
+                    return
+                #3 add the text
+                # text = str(val) + "%"
+                col_number = editable_cells[widget]
+                model[path][col_number] = text
 
-                def edited_cb(cell, path, new_text, user_data):
-                    print "edited aa"
+            # Call signal
+            cell.connect("edited", text_edited)
 
-                editable_cells[cell] = i
+        print editable_cells
+        # the column is created
+        col = Gtk.TreeViewColumn(table_titles[i], cell, text=i)
 
-                ## Event for Edited cells
-                def text_edited(widget, path, text):
-                    #1 remove the % symbol and spaces
-                    # text = text.replace("%","")
-                    # text = text.replace(" ","")
-                    print text
-                    #2 validate the number between 0 and 100
-                    val = float(text)
-                    if val < 0 or val >100:
-                        return
-                    #3 add the text
-                    # text = str(val) + "%"
-                    col_number = editable_cells[widget]
-                    model[path][col_number] = text
+        # append to the treeview
+        view.append_column(col)
 
-                # Call signal
-                cell.connect("edited", text_edited)
-
-            # the column is created
-            col = Gtk.TreeViewColumn(table_titles[i], cell, text=i)
-
-            # append to the treeview
-            view.append_column(col)
-
-
-        # FILL the table
-
-        # when a row is selected, it emits a signal
-        # view.get_selection().connect("changed", self.on_changed)
-
-        # the label we use to show the selection
-        self.label = Gtk.Label()
-        self.label.set_text("fads")
-
-        # a grid to attach the widgets
-        grid = Gtk.Grid()
-        grid.attach(view, 0, 0, 1, 1)
-        grid.attach(self.label, 0, 1, 1, 1)
-
-        # attach the grid to the window
-        self.add(grid)
-
-    def on_changed(self, selection):
-        # get the model and the iterator that points at the data in the model
-        (model, iter) = selection.get_selected()
-        # set the label to a new value depending on the selection
-        self.label.set_text("\n %s %s %s" % (model[iter][0], model[iter][1], model[iter][2]))
-        print "changed"
-        return True
-
-
+    return view
--- test/cpttable_test.py
+++ test/cpttable_test.py
@@ -13,16 +13,36 @@ class MyApplication(Gtk.Application):
 
     def do_activate(self):
         # Graph
-        vertices = ["A", "B", "C"]
-        edges = [["A", "B"], ["C", "B"]]
-        states = {}
-        states["A"] = ["atrue", "afalse"]
-        states["B"] = ["true", "false"]
-        states["C"] = ["ctrue", "afalse"]
+        vertices = ["A", "B", "C", "D"]
+        edges = [["A", "B"], ["C", "B"], ["D", "B"]]
+
+        states = {"A": ["atrue", "afalse"],
+                  "B": ["true", "false"],
+                  "C": ["ctrue", "cfalse"],
+                  "D": ["1", "2", "3"]}
 
         cpt_v = "B"
-        win = gwidgets.CptTable(self, vertices, edges, states, cpt_v)
-        win.show_all()
+        #table = gwidgets.CptTable(self, vertices, edges, states, cpt_v)
+        table = gwidgets.create_treeview_for_cpt(vertices, edges, states, cpt_v)
+
+
+        ## Window
+        window = Gtk.Window(application=self, title="CPT for " + cpt_v)
+        window.set_default_size(250, 100)
+        window.set_border_width(10)
+
+        # the label we use to show the selection
+        label = Gtk.Label()
+        label.set_text("fads")
+
+        # a grid to attach the widgets
+        grid = Gtk.Grid()
+        grid.attach(table, 0, 0, 1, 1)
+        grid.attach(label, 0, 1, 1, 1)
+
+        # attach the grid to the window
+        window.add(grid)
+        window.show_all()
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
