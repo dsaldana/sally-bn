8bea5ad717beeb882dd97f51f917453d59effcfb
dav <fds>
2014-04-16 21:55:28 -0300

    dividing glade files

--- lib_sallybn/util/resources.py
+++ lib_sallybn/util/resources.py
@@ -1,5 +1,5 @@
 
 
 # Glade Files
-MAIN_WINDOW_GLADE = 'resources/main_window.glade'
-DISC_VAR_DIALOG_GLADE = 'resources/discrete_var_dialog.glade'
+MAIN_WINDOW_GLADE = 'resources/gui/main_window.glade'
+DISC_VAR_DIALOG_GLADE = 'resources/gui/discrete_var_dialog.glade'
